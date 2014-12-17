5694aaa673d8da16c730c43a62d13483ad419ee2
XBWer <max_xbw@163.com>
2014-12-18 00:47:20 +0800

    Demo
    demo

---
+++ src/com/drools/demo/point/RuleBaseFacatory.java
@@ -0,0 +1,15 @@
+package com.drools.demo.point;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+
+/**
+ * RuleBaseFacatory 单实例RuleBase生成工具
+ */
+public class RuleBaseFacatory {
+	private static RuleBase ruleBase;
+	
+	public static RuleBase getRuleBase(){
+		return null != ruleBase ? ruleBase : RuleBaseFactory.newRuleBase();
+	}
+}
